133208692936db210b6f8a9ad2ed0069c7efce45
yaider <yaidercc@gmail.com>
2020-12-04 16:19:33 -0500

    estructura html hecha

--- Reservas.php
+++ Reservas.php
@@ -9,63 +9,80 @@
      <!-- jQuery and JS bundle w/ Popper.js -->
      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
+     <script src="https://kit.fontawesome.com/2efdabf6ca.js" crossorigin="anonymous"></script>
      <link rel="stylesheet" href="Css/normalice.css" crossorigin="anonymous">
      <link rel="stylesheet" href="Css/Estilos.css" crossorigin="anonymous">
-
+     <script src="js/Funciones.js"></script>
      <title>Reservar</title>
 </head>
 
 <body>
-     <div class="container">
-          <div class="container-formulario">
-               <form action="" class="formulario">
-                    <div class="inicio">
-                         <h1>Reservar</h1>
-                         <div class="inputs">
-                              <label >Buscar Su documento de identidad</label>
-                              <input type="text" class="form-control" id="exampleInputEmail1" placeholder="ingrese cedula" required>
-                              <span></span>
-                         </div>
-                    </div>
-                    <div class="centrales">
-                         <div class="columa-izquierda">
-                              <div class="inputs">
-                                   <label for="exampleInputEmail1">Nombres</label>
-                                   <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Su nombre" readonly>
-                              </div>
-                              <div class="inputs">
-                                   <label for="exampleInputEmail1">fecha de reserva</label>
-                                   <input type="date" class="form-control" id="exampleInputEmail1" placeholder="fecha" required>
-                              </div>
-                              <input type="submit" class="btn btn-primary" value="Ver modulos">
-                              <span>Modulo: </span>
-                         </div>
-                         <div class="columna-derecha">
-                              <div class="inputs">
-                                   <label for="exampleInputEmail1">Apellidos</label>
-                                   <input type="text" class="form-control" id="exampleInputEmail1" placeholder="su apellido" readonly>
-                              </div>
-                              <div class="Horarios">
-                                   <div class="inputs">
-                                        <label for="exampleInputEmail1">Hora de Inicio</label>
-                                        <input type="time" class="hinicio form-control" id="exampleInputEmail1" placeholder="9:00 am" required>
-                                   </div>
-                                   <div class="inputs">
-                                        <label for="exampleInputEmail1">Hora final</label>
-                                        <input type="time" class="fin form-control" id="exampleInputEmail1" placeholder="16:00 pm" required>
-                                   </div>
-                              </div>
-                              
-                         </div>
-                    </div>
-                    <div class="btn-reserva">
-                         <input type="submit" class="btn btn-primary" value="Reservar">
-                    </div>
-               </form>
-          </div>
+     <div class="section-1 container-login">
+          <form action="">
+               <h1>iniciar sesion</h1>
+               <div class="form-group">
+                    <label class="col-lg-6 control-label">Ingrese su numero de documento</label>
+                    <input type="text" placeholder="cedula" class="form-control">
+               </div>
+               <input type="submit" class="btn-primary" value="Ingresar">
+          </form>
      </div>
-     <form>
+     <div class="section-2 form-container">
+          <form action="php/ingresar.php">
+               <h1>Reservar</h1>
+               <div class="container-inputs">
+                    <div class="form-group">
+                         <label class="col-lg-6 control-label"><span class="text-title">tus nombres</span><span class="text-danger"></span></label>
+                         <input type="text" class="form-control">
+                    </div>
+                    <div class="form-group">
+                         <label class="col-lg-6 control-label"><span class="text-title">tus apellidos</span><span class="text-danger"></span></label>
+                         <input type="text" class="form-control">
+                    </div>
+
+                    <div class="form group">
+                         <label class="col-lg-6 control-label"><span class="text-title">tus apellidos</span><span class="text-danger"></span></label>
+                         <input type="text" class="form-control">
+                    </div>
+
+                    <div class="form group">
+                         <label class="col-lg-6 control-label"><span class="text-title">fecha de reserva</span><span class="text-danger">*</span></label>
+                         <input type="date" class="form-control">
+                    </div>
+
+                    <div class="form group">
+                         <label class="col-lg-6 control-label"><span class="text-title">hora inicio</span><span class="text-danger">*</span></label>
+                         <input type="time" class="form-control">
+                    </div>
+
+                    <div class="form group">
+                         <label class="col-lg-6 control-label"><span class="text-title">hora final</span><span class="text-danger">*</span></label>
+                         <input type="time" class="form-control">
+                    </div>
+
+                    <div class="form group">
+                         <input type="submit" class="btn-primary" value="Elejir Modulo">
+                         <span>Modulo: </span>
+                    </div>
+               </div>
+               <input type="submit" class="btn-primary" value="Reservar">
+
+          </form>
+     </div>
+
+     <div class="overlay-modules">
+          <div class="popup">
+               <div class="radio-modulo">
+                    <h1>Modulos</h1>
+                    <input type="radio">
+                    <span class="is-valid">Modulo 30<span>
+                              <label><span class="fas fa-check-circle"></span><span>Disponible</span></label>
+               </div>
+               <input type="submit" class="btn-primary" value="Elejir">
+          </div>
+
+     </div>
+
 </body>
 
 </html>
---
+++ php/conexion.php
@@ -0,0 +1,9 @@
+<?php
+     $conexion=mysqli_connect("localhost","root","","reservas_staff_bd");
+     if($conexion){
+          echo "melo";
+     }else{
+          echo "melo";
+     }
+
+?>
